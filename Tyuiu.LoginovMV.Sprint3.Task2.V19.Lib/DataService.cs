472fc8b90eb0f205cd29c41908441607217487da
Максим Логинов <maxdompc@gmail.com>
2023-10-18 21:52:56 +0500

    Task2

---
+++ Tyuiu.LoginovMV.Sprint3.Task2.V19.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.LoginovMV.Sprint3.Task2.V19.Lib
+{
+    public class DataService : ISprint3Task2V19
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double sumValue = 0;
+            do
+            {
+                sumValue += (Math.Cos(startValue) * 1/2);
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(sumValue, 3);
+        }
+    }
+}
---
+++ Tyuiu.LoginovMV.Sprint3.Task2.V19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint3.Task2.V19.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint3.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                                  *");
+            Console.WriteLine("* Тема: Оператор цикла do-while                                                                              *");
+            Console.WriteLine("* Задание #2                                                                                                 *");
+            Console.WriteLine("* Вариант #19                                                                                                *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Написать программу используя цикл do-while, которая вычисляет сумму ряда по формуле                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            int startValue = 1;
+            int stopValue = 10;
+            Console.WriteLine("старт шага:" + startValue);
+            Console.WriteLine("конец шага:" + stopValue);
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Результат:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            double res = ds.GetSumSeries(startValue, stopValue);
+            Console.WriteLine("Сумма ряда =" + res);
+            Console.ReadKey();
+        }
+    }
+}
