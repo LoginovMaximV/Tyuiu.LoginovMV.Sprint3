10d4d8abc0aaf14218089d7597c394c0f4dfede0
Максим Логинов <maxdompc@gmail.com>
2023-10-25 21:18:30 +0500

    Task5

---
+++ Tyuiu.LoginovMV.Sprint3.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LoginovMV.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LoginovMV.Sprint3.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db1fe6ac-895c-4c6e-9a8f-ef8bf4b64c8c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LoginovMV.Sprint3.Task5.V20/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint3.Task5.V20.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint3.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                                  *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                                                      *");
+            Console.WriteLine("* Задание #5                                                                                                 *");
+            Console.WriteLine("* Вариант #20                                                                                                *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Найти сумму ряда с помощью вложенного цикла, при х = 5                                                     *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 6;
+            Console.WriteLine("Значение х:" + x);
+            Console.WriteLine("Старт шага первого цикла:" + startValue1);
+            Console.WriteLine("Старт шага второго цикла:" + startValue2);
+            Console.WriteLine("Конец шага первого цикла:" + stopValue1);
+            Console.WriteLine("Конец шага второго цикла:" + stopValue2);
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Результат:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            Console.WriteLine("Сумма ряда =" + res);
+            Console.ReadKey();
+
+        }
+    }
+}
