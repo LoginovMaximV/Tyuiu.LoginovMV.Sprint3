4bdbbb2ff582c1a589bac6c8e486622bc346d720
Максим Логинов <maxdompc@gmail.com>
2023-10-27 11:08:42 +0500

    Task6

---
+++ Tyuiu.LoginovMV.Sprint3.Task6.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LoginovMV.Sprint3.Task6.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LoginovMV.Sprint3.Task6.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5964c618-6625-4a9b-8a92-e386a8c069d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.LoginovMV.Sprint3.Task6.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.LoginovMV.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.LoginovMV.Sprint3.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3| Выполнил: Логинов М.В | ИИПб-23-2";
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                                                  *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                                                   *");
+            Console.WriteLine("* Задание #6                                                                                                 *");
+            Console.WriteLine("* Вариант #3                                                                                                 *");
+            Console.WriteLine("* Выполнил: Логинов М.В   | ИИПб-23-2                                                                        *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                   *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [13, 19]               *");
+            Console.WriteLine("* сумму всех делителей больше 8                                                                              *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                           *");
+            Console.WriteLine("**************************************************************************************************************");
+            int startValue = 13;
+            int stopValue = 19;
+            Console.WriteLine("Начало отрезка:" + startValue);
+            Console.WriteLine("Конец отрезка:" + stopValue);
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("* Результат:                                                                                                 *");
+            Console.WriteLine("**************************************************************************************************************");
+            Console.WriteLine("Сумма делителей больших 8 = " + ds.GetSumTheDivisors(startValue,stopValue));
+            Console.ReadKey();
+        }
+    }
+}
